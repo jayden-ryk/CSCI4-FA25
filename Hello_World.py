9de12ee1bc6e0e5e2baa3139e13a1e89bc870a25
jayden-ryk <jrykola001@student.butte.edu>
2025-09-23 17:46:57 +0000

    add Hello_world.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,11 +1,23 @@
-# python hello world
+# python code
 
 hello = "world"
 
 print("hello", hello)
 
-this_number ="42"
+this_number = 42
 
 print (5)
 
-print(this_number) + 5
+print(this_number + 5)
+
+import array as arr
+
+a = arr.array(
+    'h', # opcode for unsigned short
+    [26] # list
+)
+b = arr.array('l', [1, 2, 3, 4, 5])
+
+print(b.pop())
+
+print('hello world')
