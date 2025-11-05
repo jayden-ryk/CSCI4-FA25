869e529b9734574eacb090cfae8629ed7e4fdafe
jayden-ryk <jrykola001@student.butte.edu>
2025-11-05 21:03:09 +0000

    Today I made astack and briefly explained it.

--- Hello_World2.py
+++ Hello_World2.py
@@ -4,9 +4,10 @@ head = 0
 def tail(q):
         return len(q)
 
-def push(q, e):wd
+def push(q, e):
     q.append(e)
     return 2
 
 def pop(q):
         return q.pop(0)
+
---
+++ Hello_World3.py
@@ -0,0 +1,12 @@
+s = []
+head = 0
+
+def tail(s):
+        return len(s)
+
+def push(s, e):
+    s.append(e)
+    return 2
+
+def pop(s):
+        return s.pop()
